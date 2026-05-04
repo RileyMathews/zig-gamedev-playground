4a17b1180f149676cadff69e1032fcda04cf4ca6
Riley Mathews <rileymathews80@gmail.com>
2026-05-04 01:04:25 -0500

    reset

--- src/main.zig
+++ src/main.zig
@@ -1,38 +1,10 @@
-const glfw = @import("zglfw");
 const std = @import("std");
-const zopengl = @import("zopengl");
-
-fn keyCallback(window: *glfw.Window, key: glfw.Key, scancode: c_int, action: glfw.Action, mods: glfw.Mods) callconv(.c) void {
-    std.debug.print("{any}\n", .{window});
-    std.debug.print("{any}\n", .{key});
-    std.debug.print("{any}\n", .{scancode});
-    std.debug.print("{any}\n", .{action});
-    std.debug.print("{any}\n", .{mods});
-}
 
 pub fn main() !void {
-    try glfw.init();
-    defer glfw.terminate();
+    var stdout_buffer: [1024]u8 = undefined;
+    var stdout_writer = std.fs.File.stdout().writer(&stdout_buffer);
+    const stdout = &stdout_writer.interface;
 
-    const window = try glfw.createWindow(600, 600, "zig-gamedev: minimal_glfw_gl", null, null);
-    defer window.destroy();
-
-    glfw.makeContextCurrent(window);
-    glfw.swapInterval(0);
-
-    try zopengl.loadCoreProfile(glfw.getProcAddress, 4, 0);
-
-    const gl = zopengl.bindings;
-
-    const clear_color = [_]f32{ 0.2, 0.4, 0.8, 1.0};
-
-    _ = window.setKeyCallback(keyCallback);
-
-    while (!window.shouldClose()) {
-        glfw.pollEvents();
-
-        gl.clearBufferfv(gl.COLOR, 0, clear_color[0..].ptr);
-
-        window.swapBuffers();
-    }
+    try stdout.print("hello world\n", .{});
+    try stdout.flush();
 }
